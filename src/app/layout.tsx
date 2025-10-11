5cec66c3931f80cca4590711d30f0cad47119cba
silvavini9 <silvavinicius2018@gmail.com>
2025-10-11 13:39:14 -0400

    feat: gera metadata dado a localizacao da pagina, ingles ou portugues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,66 +9,72 @@ import { siteDetails } from '@/data/siteDetails'
 import './globals.css'
 import { Locale } from '@/locales'
 import { NextIntlClientProvider, useMessages } from 'next-intl'
+import { getLocale, getTranslations } from 'next-intl/server'
 
 const manrope = Manrope({ subsets: ['latin'] })
 const sourceSans = Source_Sans_3({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: siteDetails.metadata.title,
-  description: siteDetails.metadata.description,
-  keywords: [
-    `${siteDetails.siteName}`,
-    'app',
-    'bloqueio',
-    'conteúdo',
-    'pornografia',
-    'hábitos',
-    'foco',
-    'controle',
-    'vida',
-  ],
-  authors: [{ name: siteDetails.siteName }],
-  creator: siteDetails.siteName,
-  publisher: siteDetails.siteName,
-  icons: {
-    icon: [{ url: '/favicon.ico', sizes: 'any' }],
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
+export async function generateMetadata(): Promise<Metadata> {
+  const t = await getTranslations('globals.metadata')
+  const locale = await getLocale()
+
+  return {
+    title: t('title'),
+    description: t('description'),
+    keywords: [
+      `${siteDetails.siteName}`,
+      'app',
+      'bloqueio',
+      'conteúdo',
+      'pornografia',
+      'hábitos',
+      'foco',
+      'controle',
+      'vida',
+    ],
+    authors: [{ name: siteDetails.siteName }],
+    creator: siteDetails.siteName,
+    publisher: siteDetails.siteName,
+    icons: {
+      icon: [{ url: '/favicon.ico', sizes: 'any' }],
+    },
+    robots: {
       index: true,
       follow: true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  openGraph: {
-    title: siteDetails.metadata.title,
-    description: siteDetails.metadata.description,
-    url: siteDetails.siteUrl,
-    type: 'website',
-    locale: siteDetails.locale,
-    siteName: siteDetails.siteName,
-    images: [
-      {
-        url: '/images/diaum-logo.png',
-        width: 1200,
-        height: 630,
-        alt: `${siteDetails.metadata.title}`,
+      googleBot: {
+        index: true,
+        follow: true,
+        'max-video-preview': -1,
+        'max-image-preview': 'large',
+        'max-snippet': -1,
       },
-    ],
-  },
-  twitter: {
-    card: 'summary_large_image',
-    title: siteDetails.metadata.title,
-    description: siteDetails.metadata.description,
-    images: ['/images/diaum-logo.png'],
-  },
-  alternates: {
-    canonical: siteDetails.siteUrl,
-  },
+    },
+    openGraph: {
+      title: t('title'),
+      description: t('description'),
+      url: siteDetails.siteUrl,
+      type: 'website',
+      locale: locale,
+      siteName: siteDetails.siteName,
+      images: [
+        {
+          url: '/images/diaum-logo.png',
+          width: 1200,
+          height: 630,
+          alt: `${t('title')}`,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: t('title'),
+      description: t('description'),
+      images: ['/images/diaum-logo.png'],
+    },
+    alternates: {
+      canonical: siteDetails.siteUrl,
+    },
+  }
 }
 
 interface RootLayoutProps {
--- src/data/siteDetails.ts
+++ src/data/siteDetails.ts
@@ -1,13 +1,6 @@
 export const siteDetails = {
   siteName: 'diaum',
   siteUrl: 'https://diaum.app/',
-  metadata: {
-    title: 'diaum - App de Bloqueio de Conteúdo | Livre, Focado e no Controle da Sua Vida',
-    description:
-      'Transforme seus hábitos com o diaum - o app que bloqueia pornografia e conteúdo inadequado. Seja livre, focado e no controle da sua vida desde o Dia Um. Download gratuito!',
-  },
-  language: 'pt-br',
-  locale: 'pt-BR',
   siteLogo: `${process.env.BASE_PATH || ''}/images/diaum-logo.png`,
   googleAnalyticsId: '',
 }
