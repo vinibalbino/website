16e4ae64f2e54dc27ea068b7204d60e532b8ec94
Horacio <contato@java10x.dev>
2025-10-12 11:15:47 -0300

    fix: configure Next.js images for Notion domains
    - Add remotePatterns for notion.so domains
    - Add support for AWS S3 domains (common for Notion images)
    - Fix image-unconfigured-host error when loading Notion images
    - Allow external image loading from Notion CMS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.notion.so',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.s3.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
 
 export default nextConfig
